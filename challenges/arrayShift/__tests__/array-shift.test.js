e15193624215355de26b12704e32ced16057f8e0
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-05-18 16:39:58 +0300

    eslint

--- challenges/arrayShift/__tests__/array-shift.test.js
+++ challenges/arrayShift/__tests__/array-shift.test.js
@@ -2,7 +2,7 @@
 const insertShiftArray = require('../array-shift.js');
 
 describe('Shift array', () => {
-    test('It should put the passed value in the correct order in the array', () => {
-        expect(insertShiftArray([2, 4, 6, 8], 5)).toStrictEqual([2, 4, 5, 6, 8]);
-    });
+  test('It should put the passed value in the correct order in the array', () => {
+    expect(insertShiftArray([2, 4, 6, 8], 5)).toStrictEqual([2, 4, 5, 6, 8]);
+  });
 });
