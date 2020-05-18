0b9142e7445a6400b28195d28ee35e5e4adc652a
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-05-18 16:08:09 +0300

    challenge 2 done without read me yet

---
+++ challenges/arrayShift/__tests__/array-shift.test.js
@@ -0,0 +1,8 @@
+'use strict';
+const insertShiftArray = require('../array-shift.js');
+
+describe('Shift array', () => {
+    test('It should put the passed value in the correct order in the array', () => {
+        expect(insertShiftArray([2, 4, 6, 8], 5)).toStrictEqual([2, 4, 5, 6, 8]);
+    });
+});
