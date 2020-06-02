afbbe61a8fb63d0b2bac142c7f0ffe98b54c3ab6
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-06-03 00:21:34 +0300

    done with console testing only

--- challenges/Data-Structures/linkedList/linked-list.js
+++ challenges/Data-Structures/linkedList/linked-list.js
@@ -127,15 +127,41 @@ class LinkedList {
 
 }
 
-const linkedListInstance = new LinkedList();
-linkedListInstance.append(1);
-linkedListInstance.append(2);
-linkedListInstance.append(3);
-linkedListInstance.append(5);
-console.log(linkedListInstance.insertAfter(3,4));
-console.log(linkedListInstance.toString());
-console.log(linkedListInstance.kthFromEnd(0));
+function mergeLists(linkedList1, linkedList2){
+  let first = linkedList1.head.value;
+  let second = linkedList2.head.value;
+  let firstNewNode = new Node(first);
+  let newLinkedList = new LinkedList();
+  newLinkedList.head = firstNewNode;
+  newLinkedList.append(second); 
 
+  while((linkedList1.head.next) || ( linkedList2.head.next)){
+    if (linkedList1.head.next){
+      let val = linkedList1.head.next.value;
+      newLinkedList.append(val);
+    }
+    if(linkedList2.head.next){
+      let val = linkedList2.head.next.value;
+      newLinkedList.append(val);
+    }  
+    linkedList1 = {head: linkedList1.head.next};
+    linkedList2 = {head: (linkedList2.head.next)};
+    console.log('check!', linkedList2);
+  }
+  return newLinkedList;
+}
+
+const linkedListInstance1 = new LinkedList();
+linkedListInstance1.append(1);
+linkedListInstance1.append(3);
+linkedListInstance1.append(2);
+const linkedListInstance2 = new LinkedList();
+linkedListInstance2.append(5);
+linkedListInstance2.append(9);
+linkedListInstance2.append(4);
+
+let newLinked = mergeLists(linkedListInstance1,linkedListInstance2);
+console.log('hello!!!!', newLinked.toString());
 
 module.exports = LinkedList;
 
