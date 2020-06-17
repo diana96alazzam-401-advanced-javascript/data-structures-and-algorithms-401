5c494b3b90e35669e5b02bb540cf86220f9b165a
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-06-17 23:44:52 +0300

    add fix

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -1,10 +1,27 @@
 'use strict';
 
+const util = require('util');
+
 class Node {
   constructor(value, right = null, left = null) {
     this.value = value;
     this.right = right;
     this.left = left;
+  }
+  binaryAddNode(node){
+    if(node.value<this.value){
+      if(this.left === null){
+        this.left = node;
+      } else {
+        this.left.binaryAddNode(node);
+      }
+    } else if (node.right>this.value) {
+      if (this.right === null) {
+        this.right = node;
+      } else {
+        this.right.binaryAddNode(node);
+      }
+    }
   }
 }
 class BinaryTree {
@@ -48,125 +65,34 @@ class BinaryTree {
 }
 
 class BinarySearchTree {
-  constructor(node) {
-    this.tree = new BinaryTree(node);
+  constructor() {  
+    this.root = null;
   }
   add(val) {
     let newNode = new Node(val);
-    let _traversal = (node) => {
-      if ((node.right)&&(node.right.value > val)){
-        _traversal(node.left);
-      } else if ((node.right)&&(node.right.value < val)){
-        _traversal(node.right);
-      }
-    };
-    _traversal(this.tree.root);
-    return results;
-
+    if (this.root === null){
+      this.root = newNode;
+    } else {
+      this.root.binaryAddNode(newNode);
+    }
   }
-  // contains (value) {
-
-
-  // }
 }
 
-// class BinaryTree {
-//   constructor(root) {
-//     this.root = root;
-//   }
-//   preOrder() {
-//     //   root - left - right
-//     let results = [];
-//     let _traversal = (node) => {
-//       results.push(node.value);
-//       if (node.left) _traversal(node.left);
-//       if (node.right) _traversal(node.right);
-//     };
-//     _traversal(this.root);
-//     return results;
-//   }
-//   inOrder() {
-//     //   left - root - right
-//     let results = [];
-//     let _traversal = (node) => {
-//       if (node.left) _traversal(node.left);
-//       results.push(node.value);
-//       if (node.right) _traversal(node.right);
-//     };
-//     _traversal(this.root);
-//     return results;
-//   }
-//   postOrder() {
-//     //   left - right - root
-//     let results = [];
-//     let _traversal = (node) => {
-//       if (node.left) _traversal(node.left);
-//       if (node.right) _traversal(node.right);
-//       results.push(node.value);
-//     };
-//     _traversal(this.root);
-//     return results;
-//   }
-// }
-
-// class BinarySearchTree {
-//   constructor(root) {
-//     this.root = root;
-//   }
-//   add(val) {
-//     var newNode = new Node(val);
-
-//     if (this.root === null)
-//       this.root = newNode;
-//     else {
-//       this.insertNode(this.root, newNode);
-//     }
-//   }
 
 
-//   insertNode(node, newNode) {
-//     if (newNode.data < node.data) {
-//       if (node.left === null) {
-//         node.left = newNode;
-//       } else {
-//         this.insertNode(node.left, newNode);
-//       }
-//     } else {
-//       if (node.right === null) {
-//         node.right = newNode;
-//       }
-//       else {
-//         this.insertNode(node.right, newNode);
-//       }
-//     }
-//   }
-
-//   //   contains(){
-
-//   //   }
-// }
-
-const five = new Node(5);
-const fifteen = new Node(2);
-const three = new Node(3);
-
-
-five.left = three;
-five.right = fifteen;
-// two.left = three;
-// two.right = six;
-// three.left = eight;
-// six.right = nine;
-// four.left = one;
-// four.right = seven;
-// seven.left = ten;
-// seven.right = twentyTwo;
 
 
 
 // let tree = new BinaryTree(five);
-let newBinarySearchTree = new BinarySearchTree(five);
-newBinarySearchTree.add(1);
+let newBinarySearchTree = new BinarySearchTree();
+for (let i = 1; i<10; i++){
+  newBinarySearchTree.add(i);
+}
+
+
+console.log(util.inspect(newBinarySearchTree, false, null, true));
+
+// console.log(newBinarySearchTree);
 
 
 module.exports = {
