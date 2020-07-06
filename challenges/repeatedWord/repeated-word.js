a83f4556ea0ef829039e0ca8416a83b456d10017
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-07-06 09:53:48 +0300

    whiteboard and read me

--- challenges/repeatedWord/repeated-word.js
+++ challenges/repeatedWord/repeated-word.js
@@ -32,43 +32,50 @@ class Hashmap {
     const hashKey = (sumCharCode * 19) % this.size;
     return hashKey;
   }
-  add(key, value) {
-    const hash = this.hash(key);
-    if (!this.storage[hash]) {
-      const ll = new LinkedList();
-      ll.prepend({ [key]: value });
-      this.storage[hash] = ll;
-    } else {
-      this.storage[hash].prepend({ [key]: value });
-    }
-  }
-
-  get(key) {
-    const hash = this.hash(key);
-    if (this.storage[hash]) {
-      return this.storage[hash];
-    } else {
-      return 'no value for this key';
-    }
-
-  }
-  contain(key) {
-    const hash = this.hash(key);
-    if (this.storage[hash]) {
-      return true;
-    } else {
-      return false;
-    }
-
-  }
 }
 
 function repeatedWords(words){
-  let hashmap = new Hashmap(97);
-  let splitted = words.split(' '); //check if there is ,;?!
-  splitted.forEach(word=> {
-  let hashedWord = hashmap.hash(word);
-    hashmap.add(word);
-  })
-
+  let hashmap = new Hashmap(4000);
+  let splitted = words.toLowerCase().split(' '); //check if there is ,;?!
+  let loop = false;
+  let idx = 0;
+  while(!loop){
+    let hashed = hashmap.hash(splitted[idx].split(',')[0]);
+    if (!hashmap.storage[hashed]) {
+      const ll = new LinkedList();
+      ll.prepend({ [hashed]: splitted[idx].split(',')[0] });
+      hashmap.storage[hashed] = ll;
+      idx++;
+    } else {
+      loop = true;
+    }
+  }
+  return splitted[idx].split(',')[0];
 }
+
+// suggested way of removing the punctuations
+
+// let noPunc = word.split(',').join('');
+// noPunc = noPunc.split('.').join('');
+// noPunc = noPunc.split(';').join('');
+// noPunc = noPunc.split('!').join('');
+// noPunc = noPunc.split('?').join('');
+// noPunc = noPunc.split(':').join('');
+// noPunc = noPunc.split('(').join('');
+// noPunc = noPunc.split(')').join('');
+// noPunc = noPunc.split('{').join('');
+// noPunc = noPunc.split('}').join('');
+// noPunc = noPunc.split('[').join('');
+// noPunc = noPunc.split(']').join('');
+
+
+let words= 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York';
+
+let words1= 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
+
+let words2= 'Once upon a time, there was a brave princess who...';
+
+
+console.log(repeatedWords(words));
+console.log(repeatedWords(words1));
+console.log(repeatedWords(words2));
